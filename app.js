e8193e3a0c824ba318d796e59d4d6debaf47a862
Salman Fareed <101673229+salmanfahmad@users.noreply.github.com>
2024-06-27 23:21:06 +0500

    Removed console.logs

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const app = express();
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     const uploadPath = path.join(__dirname, 'uploads');
-    console.log('Upload path:', uploadPath);
+
     cb(null, uploadPath); // Uploads folder where images will be stored
   },
   filename: function(req, file, cb) {
     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
-    console.log('Generated filename:', uniqueSuffix);
+
     cb(null, `${file.fieldname}-${uniqueSuffix}`); // Unique filename
   }
 });
@@ -27,10 +27,10 @@ const upload = multer({
 // API endpoint to handle file uploads
 app.post('/upload', upload.single('image'), (req, res) => {
   try {
-    console.log('Received file:', req.file);
+
 
     if (!req.file) {
-      console.log('No file uploaded');
+
       return res.status(400).json({ success: false, message: 'No file uploaded' });
     }
 
@@ -39,7 +39,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
     const filename = req.file.filename;
     const fileUrl = `${protocol}://${host}/uploads/${filename}`;
 
-    console.log('File uploaded successfully:', req.file.filename);
+
     return res.status(201).json({
       success: true,
       message: 'File uploaded successfully',
@@ -47,7 +47,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
       path: fileUrl
     });
   } catch (error) {
-    console.error('Error uploading file:', error);
+
     return res.status(500).json({ success: false, message: 'Error uploading file' });
   }
 });
@@ -58,17 +58,16 @@ app.get('/uploads/:filename', (req, res) => {
     const { filename } = req.params;
     const filePath = path.join(__dirname, 'uploads', filename);
 
-    console.log('Requested image:', filename);
 
     if (!fs.existsSync(filePath)) {
-      console.log('File not found:', filename);
+
       return res.status(404).json({ success: false, message: 'File not found' });
     }
 
-    console.log('Sending file:', filePath);
+
     res.sendFile(filePath);
   } catch (error) {
-    console.error('Error retrieving image:', error);
+
     return res.status(500).json({ success: false, message: 'Error retrieving image' });
   }
 });
